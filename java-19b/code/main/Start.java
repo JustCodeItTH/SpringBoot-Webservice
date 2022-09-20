50c54a91fced70512577c7a2eae1f4c0d3a0a75f
Nopporn Pirayawaraporn <106856494+nopporn-piraya@users.noreply.github.com>
2022-09-20 16:02:21 +0700

    Add files via upload

---
+++ java-19b/code/main/Start.java
@@ -0,0 +1,23 @@
+package main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+class Start {
+    public static void main(String[] data) {
+        ApplicationContext context;
+        context = SpringApplication.run(Setup.class);
+    }
+}
+@SpringBootApplication
+class Setup { }
+
+@RestController
+class Sample {
+    @RequestMapping("/check")
+    String show() {
+        return "I Love You";
+    }
+}
